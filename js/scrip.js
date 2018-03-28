1da4b9fbc2f9bf4953d337627fd097c355897cff
Vincent KOWALSKI <vincent.kowalski@etu.univ-nantes.fr>
2018-03-28 13:51:18 +0200

    première mise à jour

---
+++ js/scrip.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function() {
+  $(window).stellar();
+  $('#myCarousel').carousel({
+    interval: 40000
+  });
+
+  $("html").niceScroll({
+    cursorcolor:"rgba(30,30,30,.5)",
+    zindex:999,
+    scrollspeed:100,
+    mousescrollstep:50,
+    cursorborder:"0px solid #fff",
+  });
+
+
+
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+    duration:200,
+    numVisible:3
+  });
+
+
+  autoplay();
+
+  // move next carousel
+  $('.moveNextCarousel').click(function(e){
+     e.preventDefault();
+     e.stopPropagation();
+     $('.carousel').carousel('next');
+  });
+
+  // move prev carousel
+  $('.movePrevCarousel').click(function(e){
+     e.preventDefault();
+     e.stopPropagation();
+     $('.carousel').carousel('prev');
+  });
+
+
+});
+
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
